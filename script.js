abecf0e560bae8c6a4d8613568dd90d52de05b7c
Ollie Christensen <ollie.a.c@outlook.com>
2026-07-31 09:09:44 -0600

    change variable names

--- script.js
+++ script.js
@@ -13,31 +13,31 @@ async function draw() {
   const sheet = document.getElementById("source");
 
   // Draw slice
-  function drawTile (type, x, y) {
+  function drawTile (type, canvasx, canavasy) {
    switch (type) {
     case "wall":
-      ctx.drawImage(sheet, 0, 0, 30, 30, x, y, 30, 30);
+      ctx.drawImage(sheet, 0, 0, 30, 30, canvasx, canvasy, 30, 30);
       break;
     case "floor":
-      ctx.drawImage(sheet, 30, 0, 30, 30, x, y, 30, 30);
+      ctx.drawImage(sheet, 30, 0, 30, 30, canvasx, canvasy, 30, 30);
       break;
     case "moss-floor":
-      ctx.drawImage(sheet, 60, 0, 30, 30, x, y, 30, 30);
+      ctx.drawImage(sheet, 60, 0, 30, 30, canvasx, canvasy, 30, 30);
       break;
     case "water":
-      ctx.drawImage(sheet, 90, 0, 30, 30, x, y, 30, 30);
+      ctx.drawImage(sheet, 90, 0, 30, 30, canvasx, canvasy, 30, 30);
       break;
     case "warp":
-      ctx.drawImage(sheet, 0, 30, 30, 30, x, y, 30, 30);
+      ctx.drawImage(sheet, 0, 30, 30, 30, canvasx, canvasy, 30, 30);
       break;
     case "person":
-      ctx.drawImage(sheet, 30, 30, 30, 30, x, y, 30, 30);
+      ctx.drawImage(sheet, 30, 30, 30, 30, canvasx, canvasy, 30, 30);
       break;
     case "key":
-      ctx.drawImage(sheet, 60, 30, 30, 30, x, y, 30, 30);
+      ctx.drawImage(sheet, 60, 30, 30, 30, canvasx, canvasy, 30, 30);
       break;
     case "chest":
-      ctx.drawImage(sheet, 90, 30, 30, 30, x, y, 30, 30);
+      ctx.drawImage(sheet, 90, 30, 30, 30, canvasx, canvasy, 30, 30);
       break;
     default:
       console.log("something goofed");
@@ -54,6 +54,9 @@ async function draw() {
   drawTile("key", 30, 30);
   drawTile("chest", 60, 30);
 
+  function drawBackground () {
+    for 
+  }
 
 }
 
